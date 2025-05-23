de9eebacf4db58093cd179b47be23b87ab9194e3
Dayanch437 <dayanchkemalovich@gmail.com>
2025-05-23 07:31:21 +0500

    add BaseModel

--- apps/utils/models.py
+++ apps/utils/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class BaseModel(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
--- config/settings/base.py
+++ config/settings/base.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     # 'channels',
 
     #my
+    'apps.utils',
     'apps.adds',
     'apps.cart',
     'apps.store',
